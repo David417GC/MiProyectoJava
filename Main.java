96fcf0898ff5fce7315d6b2eaf894480809c5459
alumno <david1704gc@gmail.com>
2024-11-12 15:43:16 +0100

    Modificado MiArchivo.java para actualizar funcionalidades

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola, GitHub!");
+        System.out.println("Hola, GitHub! este archivo sea modificado");
     }
 }
